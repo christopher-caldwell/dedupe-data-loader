1594dae5afc294e4c3d213f67d745b36fa83f61e
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2023-04-05 14:00:10 -0500

    feat: Allowing keys to be typed (#13)

--- tests/setup/mocks.ts
+++ tests/setup/mocks.ts
@@ -4,12 +4,25 @@ export const mockFetcher = jest.fn(async (ids: Key[]): Promise<TestCacheItem[]> 
   return ids.map((id) => ({ id }))
 })
 
+export const bufferMockFetcher = jest.fn(async (ids: Buffer[]): Promise<BufferTestCacheItem[]> => {
+  return ids.map((id) => ({ id }))
+})
+
 export const TestDataLoader = new DataLoader<TestCacheItem>({
   fetcher: mockFetcher,
 })
 
+export const BufferDataLoader = new DataLoader<BufferTestCacheItem, Buffer>({
+  fetcher: jest.fn(async (ids) => {
+    return ids.map((id) => ({ id }))
+  }),
+})
+
 export type TestCacheItem = {
   id: Key
+}
+export type BufferTestCacheItem = {
+  id: Buffer
 }
 
 beforeEach(() => {
--- tests/unit/buffers.spec.ts
+++ tests/unit/buffers.spec.ts
@@ -1,15 +1,15 @@
-import { TestDataLoader } from '@setup/index'
+import { BufferDataLoader } from '@setup/index'
 
 const testId = Buffer.from('123')
 
 describe('Buffers', () => {
   test('[load] Using buffers as keys is valid', async () => {
-    const result = await TestDataLoader.load(testId)
+    const result = await BufferDataLoader.load(testId)
     expect(Buffer.compare(result.id as Buffer, testId)).toBe(0)
   })
   test('[mSet] Using buffers as keys is valid', () => {
-    TestDataLoader.mSet([{ id: testId }])
-    const result = TestDataLoader.mGet([testId])
+    BufferDataLoader.mSet([{ id: testId }])
+    const result = BufferDataLoader.mGet([testId])
     expect(result).toHaveLength(1)
   })
 })
